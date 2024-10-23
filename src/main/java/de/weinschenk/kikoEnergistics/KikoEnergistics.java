5a4b9a81ab780138c6875122b78d2bc4e3dca17d
Suyuimo <svewei8@icloud.com>
2024-10-23 23:46:21 +0200

    Add basis Files

---
+++ src/main/java/de/weinschenk/kikoEnergistics/KikoEnergistics.java
@@ -0,0 +1,17 @@
+package de.weinschenk.kikoEnergistics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KikoEnergistics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
